6eb4a5b88a44ff3c93e3ae1ea4b6325af3469675
L Abinesh <abineshl653@gmail.com>
2024-09-28 15:35:38 +0530

    3./4 Completed from the Udemy Tutorial
    Mostly Completed the Game Mechanics from the Udemy Course.

--- Assets/Scenes/Scripts/CollisionHandler.cs
+++ Assets/Scenes/Scripts/CollisionHandler.cs
@@ -3,24 +3,92 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    // Parivate
+    AudioSource audioSource;
+
+    [SerializeField] float levelLoadDelay = 3f;
+    [SerializeField] AudioClip CrashingAudio;
+    [SerializeField] AudioClip FinishAudio;
+
+    bool isTransitioning = false;
+
+    bool collisionDisable = false;
+
+    // Public
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisable = !collisionDisable; // Toggle Collision
+        }
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
+        if (isTransitioning || collisionDisable)  { return; }
+
         switch(collision.gameObject.tag)
         {
             case "Start":
                 Debug.Log("This is Start Point");
                 break;
             case "Finish":
-                Debug.Log("This is Finish Point");
+                StartSuccessSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+
      }
+
+    void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(FinishAudio);
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(CrashingAudio);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
 
     void ReloadLevel()
     {
-        SceneManager.LoadScene(0);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
+
 }
